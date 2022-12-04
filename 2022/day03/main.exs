75f12fc7bfdacccc956bbc9dfebf5e4649faac24
Isaac Sanders <651597+isaacsanders@users.noreply.github.com>
2022-12-03 21:12:24 -0600

    2022 day 03 part 02

--- 2022/day03/main.exs
+++ 2022/day03/main.exs
@@ -20,6 +20,21 @@ defmodule Day03 do
     end)
     |> Enum.reduce(&Kernel.+/2)
   end
+
+  def part02(input) do
+    input
+    |> Stream.map(&String.trim/1)
+    |> Stream.chunk_every(3)
+    |> Stream.flat_map(fn group ->
+      group
+      |> Enum.map(&(&1 |> to_charlist |> MapSet.new()))
+      |> Enum.reduce(&MapSet.intersection/2)
+      |> Enum.to_list()
+      |> Enum.map(&Map.fetch!(@priorities, &1))
+    end)
+    |> Enum.sum()
+  end
 end
 
 "./input.txt" |> File.stream!() |> Day03.part01() |> IO.inspect(label: "part 1")
+"./input.txt" |> File.stream!() |> Day03.part02() |> IO.inspect(label: "part 2")
