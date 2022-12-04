75603d9cd5c09a22e6f3ff48f3ec18fec119e101
Isaac Sanders <651597+isaacsanders@users.noreply.github.com>
2022-12-03 20:30:56 -0600

    2022 day 01 part 02

--- 2022/day01/main.exs
+++ 2022/day01/main.exs
@@ -1,12 +1,34 @@
-"./input.txt"
-|> File.stream!()
-|> Stream.map(&String.trim/1)
-|> Stream.chunk_by(&(&1 == ""))
-|> Stream.take_every(2)
-|> Stream.map(fn chunk ->
-  chunk
-  |> Enum.map(&String.to_integer/1)
-  |> Enum.sum()
-end)
-|> Enum.max()
-|> IO.inspect()
+defmodule Day01 do
+  def part1(input) do
+    input
+    |> elves()
+    |> Stream.map(&Enum.sum/1)
+    |> Enum.max()
+  end
+
+  def part2(input) do
+    input
+    |> elves()
+    |> Stream.map(&Enum.sum/1)
+    |> Enum.reduce([], fn item, top3 ->
+      [item | top3]
+      |> Enum.sort(:desc)
+      |> Enum.take(3)
+    end)
+    |> Enum.sum()
+  end
+
+  defp elves(input) do
+    input
+    |> Stream.map(&String.trim/1)
+    |> Stream.chunk_by(&(&1 == ""))
+    |> Stream.take_every(2)
+    |> Stream.map(fn chunk ->
+      chunk
+      |> Enum.map(&String.to_integer/1)
+    end)
+  end
+end
+
+"./input.txt" |> File.stream!() |> Day01.part1() |> IO.inspect(label: "part 1")
+"./input.txt" |> File.stream!() |> Day01.part2() |> IO.inspect(label: "part 2")
