473732ce67c73518d3bff4014014eb4e602a3a4c
Isaac Sanders <651597+isaacsanders@users.noreply.github.com>
2022-12-08 15:24:43 -0600

    2022 day 08 part 02

--- 2022/day08/main.exs
+++ 2022/day08/main.exs
@@ -23,7 +23,64 @@ defmodule Day08 do
   end
 
   def part02(input) do
-    input
+    trees =
+      input
+      |> Stream.map(&(&1 |> String.trim() |> String.to_charlist()))
+      |> Stream.with_index()
+      |> Stream.map(fn {line, from_top} ->
+        line
+        |> Enum.with_index()
+        |> Enum.map(fn {height, from_left} -> {height, {from_top, from_left}} end)
+      end)
+      |> Enum.to_list()
+
+    coordinate_to_height =
+      trees
+      |> List.flatten()
+      |> Map.new(fn {height, coordinate} ->
+        {coordinate, height}
+      end)
+
+    grid_height = length(trees)
+    grid_width = length(hd(trees))
+
+    internal_trees =
+      for x <- 1..(grid_width - 2), y <- 1..(grid_height - 2), into: MapSet.new() do
+        {x, y}
+      end
+
+    internal_trees
+    |> Enum.map(fn {from_top, from_left} ->
+      tree_height = coordinate_to_height[{from_top, from_left}]
+
+      scores =
+        [
+          for y <- (from_top - 1)..0 do
+            coordinate_to_height[{y, from_left}]
+          end,
+          for x <- (from_left - 1)..0 do
+            coordinate_to_height[{from_top, x}]
+          end,
+          for x <- (from_left + 1)..(grid_width - 1) do
+            coordinate_to_height[{from_top, x}]
+          end,
+          for y <- (from_top + 1)..(grid_height - 1) do
+            coordinate_to_height[{y, from_left}]
+          end
+        ]
+        |> Enum.map(fn trees ->
+          case Enum.split_while(trees, &(&1 < tree_height)) do
+            {visible_trees, []} ->
+              Enum.count(visible_trees)
+
+            {visible_trees, [_last_visible_tree | _]} ->
+              Enum.count(visible_trees) + 1
+          end
+        end)
+
+      Enum.product(scores)
+    end)
+    |> Enum.max()
   end
 
   defp visible_from_left(trees, visible_trees) do
@@ -52,13 +109,17 @@ defmodule Day08 do
 
     for source_x <- 0..(width - 1) do
       for source_y <- (height - 1)..0 do
-        trees
-        |> Enum.at(source_y)
-        |> Enum.at(source_x)
+        tree_at({source_y, source_x}, trees)
       end
     end
+  end
+
+  defp tree_at({from_top, from_left}, trees) do
+    trees
+    |> Enum.at(from_top)
+    |> Enum.at(from_left)
   end
 end
 
 "./input.txt" |> File.stream!() |> Day08.part01() |> IO.inspect(label: "part 1")
-# "./input.txt" |> File.stream!() |> Day08.part02() |> IO.inspect(label: "part 2")
+"./input.txt" |> File.stream!() |> Day08.part02() |> IO.inspect(label: "part 2")
