8fb101134ff3426cd3c7f77ba7a6856594282d85
Isaac Sanders <651597+isaacsanders@users.noreply.github.com>
2022-12-13 10:28:13 -0600

    2022 day 11 part 02

--- 2022/day11/main.exs
+++ 2022/day11/main.exs
@@ -1,11 +1,25 @@
 defmodule Day11 do
-  @operator_mappings %{"*" => &Kernel.*/2, "+" => &Kernel.+/2}
+  @arithmetic_operator_mappings %{"*" => &Kernel.*/2, "+" => &Kernel.+/2}
 
   def part01(input) do
     input
+    |> create_state()
+    |> observe_monkeys(20, &div(&1, 3))
+    |> calculate_monkey_business()
+  end
+
+  def part02(input) do
+    input
+    |> create_state()
+    |> observe_monkeys(10000, &Function.identity/1)
+    |> calculate_monkey_business()
+  end
+
+  defp create_state(input) do
+    input
     |> Stream.map(&String.trim/1)
     |> Stream.map(&parse_line/1)
-    |> Enum.reduce(%{monkeys: %{}, last_id: nil}, fn
+    |> Enum.reduce(%{constant: 1, monkeys: %{}, last_id: nil}, fn
       {:begin_monkey, id}, state ->
         state
         |> put_in([:monkeys, id], %{inspected_items: 0})
@@ -17,8 +31,10 @@ defmodule Day11 do
       {:operation, function}, state ->
         put_in(state, [:monkeys, state.last_id, :operation], function)
 
-      {:test, function}, state ->
-        put_in(state, [:monkeys, state.last_id, :test], function)
+      {:test, integer, function}, state ->
+        state
+        |> put_in([:monkeys, state.last_id, :test], function)
+        |> update_in([:constant], &(&1 * integer))
 
       {:if_true, function}, state ->
         put_in(state, [:monkeys, state.last_id, :if_true], function)
@@ -29,11 +45,9 @@ defmodule Day11 do
       :end_monkey, state ->
         state
     end)
-    |> observe_monkeys(20)
-    |> calculate_monkey_business()
   end
 
-  def parse_line(line) do
+  defp parse_line(line) do
     case line do
       "Monkey " <> rest ->
         case Integer.parse(rest) do
@@ -72,7 +86,7 @@ defmodule Day11 do
                   fn _ -> integer end
               end
 
-            operator = Map.fetch!(@operator_mappings, operation)
+            operator = Map.fetch!(@arithmetic_operator_mappings, operation)
 
             {:operation, fn old -> operator.(old, rhs_function.(old)) end}
         end
@@ -81,7 +95,12 @@ defmodule Day11 do
         case rest do
           "divisible by " <> value ->
             integer = String.to_integer(value)
-            {:test, &(rem(&1, integer) == 0)}
+
+            {:test, integer,
+             fn worry, constant ->
+               new_worry = worry - div(worry, constant) * constant
+               {new_worry, rem(new_worry, integer)}
+             end}
         end
 
       "If true: throw to monkey " <> rest ->
@@ -101,10 +120,6 @@ defmodule Day11 do
     end
   end
 
-  def part02(input) do
-    input
-  end
-
   defp throw_to(monkey_id) do
     fn state, item ->
       monkey = Map.fetch!(state.monkeys, monkey_id)
@@ -113,30 +128,32 @@ defmodule Day11 do
     end
   end
 
-  defp observe_monkeys(state, rounds_left)
+  def observe_monkeys(state, rounds_left, post_inspection)
 
-  defp observe_monkeys(state, 0) do
+  def observe_monkeys(state, 0, _post_inspection) do
     state
   end
 
-  defp observe_monkeys(state, rounds_left) when rounds_left > 0 do
+  def observe_monkeys(state, rounds_left, post_inspection) when rounds_left > 0 do
     0..state.last_id
-    |> Enum.reduce(state, &observe_round/2)
-    |> observe_monkeys(rounds_left - 1)
+    |> Enum.reduce(state, &observe_round(&1, &2, post_inspection))
+    |> observe_monkeys(rounds_left - 1, post_inspection)
   end
 
-  defp observe_round(monkey_id, state) do
+  defp observe_round(monkey_id, state, post_inspection) do
     monkey = Map.fetch!(state.monkeys, monkey_id)
 
     monkey.items
     |> :queue.to_list()
     |> Enum.reduce(state, fn item, state ->
-      new_worry = item |> monkey.operation.() |> div(3)
+      new_worry = item |> monkey.operation.() |> post_inspection.()
 
-      if monkey.test.(new_worry) do
-        monkey.if_true.(state, new_worry)
+      {reduced_worry, remainder} = monkey.test.(new_worry, state.constant)
+
+      if remainder == 0 do
+        monkey.if_true.(state, reduced_worry)
       else
-        monkey.if_false.(state, new_worry)
+        monkey.if_false.(state, reduced_worry)
       end
       |> update_in([:monkeys, monkey_id, :inspected_items], &(&1 + 1))
     end)
@@ -154,4 +171,4 @@ defmodule Day11 do
 end
 
 "./input.txt" |> File.stream!() |> Day11.part01() |> IO.inspect(label: "part 1")
-# "./input.txt" |> File.stream!() |> Day11.part02() |> IO.inspect(label: "part 2")
+"./input.txt" |> File.stream!() |> Day11.part02() |> IO.inspect(label: "part 2")
